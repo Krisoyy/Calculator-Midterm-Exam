b46d00a4fabede425c0b601f0ddb6239bce52da5
ITPC113 <ITPC113@3017STN11-101.ndmu.local>
2023-04-12 09:51:15 +0800

    first commit

---
+++ _HTML5_sign_up_form_refurbished/colorChanging.js
@@ -0,0 +1,12 @@
+
+function submitForm() {
+  // Get form data and do any necessary validation
+  var formData = document.forms[0].elements;
+  
+  // Submit form using AJAX or other method
+  // ...
+  
+  // Optional: show success message or redirect to another page
+  alert("Form submitted successfully!");
+}
+
